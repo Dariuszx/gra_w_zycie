90aadfb7775aad033b30c184fe01fb4e98b6efe7
Dariuszx <marazmad.01@gmail.com>
2014-03-23 23:50:50 +0100

    Dodałem dodatkowe pliki

---
+++ modules/lib/cellular_formation_generation.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "args.h"
+#include "mesh.h"
+#include "rules.h"
+#include "error_handling.h"
+
+error formation_generation( struct mesh* siatka, struct args* argumenty, struct rules* zasady ) {
+
+	return FINE;
+}
